57e4f1cbbd474b77d8d39e4e41ec3fa1297c51fd
Havicish <157093976+Havicish@users.noreply.github.com>
2024-09-26 15:27:07 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 let Cheese = 0;
 let Milk = 50;
+let ChocoMilk = 0;
 let Cows = 1;
 let PriceCow = 10;
 let Time = 3000;
+let CheeseMakers = 0;
+let PriceCheeseMaker = 20;
+let ChocoCows = 0;
+let PriceChocoCow = 100;
 
 function Get(Id) {
     return document.getElementById(Id);
@@ -12,16 +17,14 @@ function MakeCheeseFunc() {
     if (Milk > 0) {
         Cheese++;
         Milk--;
-        Get("CheeseCount").innerText = Cheese;
-        Get("MilkCount").innerText = Milk;
+        Get("CheeseCount").innerText = Math.round(Cheese);
+        Get("MilkCount").innerText = Math.round(Milk);
     }
 }
 
 function MilkCowFunc() {
-    if (Milk > 0) {
-        Milk += Cows;
-        Get("MilkCount").innerText = Milk;
-    }
+	Milk += Cows;
+	Get("MilkCount").innerText = Math.round(Milk);
 }
 
 function BuyCowFunc() {
@@ -34,10 +37,46 @@ function BuyCowFunc() {
     }
 }
 
+function MilkChocoCowFunc() {
+	ChocoMilk += ChocoCows;
+	Get("ChocoMilkCount").innerText = Math.round(ChocoMilk);
+}
+
+function BuyChocoCowFunc() {
+    if (Cheese >= PriceChocoCow) {
+        ChocoCows++;
+        Cheese -= PriceChocoCow;
+        PriceChocoCow += 20;
+        Get("ChocoCowCount").innerText = ChocoCows;
+        Get("PriceChocoCow").innerText = PriceChocoCow;
+    }
+}
+
+function BuyCheeseMakerFunc() {
+	if (Cheese >= PriceCheeseMaker) {
+		CheeseMakers++;
+		Cheese -= PriceCheeseMaker;
+		PriceCheeseMaker = Math.round(PriceCheeseMaker * 1.15 / 5) * 5;
+		Get("CheeseCount").innerText = Math.round(Cheese);
+		Get("CheeseMakerCount").innerText = CheeseMakers;
+		Get("PriceCheeseMaker").innerText = PriceCheeseMaker;
+	}
+}
+
 setInterval(() => {
-    Get("CheeseCount").innerText = Cheese;
-    Get("MilkCount").innerText = Milk;
+    Get("CheeseCount").innerText = Math.round(Cheese);
+    Get("MilkCount").innerText = Math.round(Milk);
+
+	if (Milk >= CheeseMakers / 10 / 2) {
+		Cheese += CheeseMakers / 10 / 2;
+		Milk -= CheeseMakers / 10 / 2;
+		Get("CheesePerSecond").innerText = CheeseMakers / 2;
+	} else {
+		Get("CheesePerSecond").innerText = Milk;
+		Cheese += Milk;
+		Milk = 0;
+	}
 
     Time--;
-    Get("TimeLeft").innerText = Time / 10;
+    Get("TimeLeft").innerText = Math.round(Time / 10);
 }, 100);
